1b1fb8624774548e7adcf09abbe55b322f392b1b
Martin Davis <martin_davis3@yahoo.ca>
2020-05-11 11:35:03 -0600

    correct filenames

--- abstraction.cs
+++ abstraction.cs
@@ -1,34 +1,21 @@
 ﻿using System;
 
-namespace encapsulation {
-    class User {
-        private string username;
-        private string password;
-        public string MyUsername {
-            get {
-                return username;
-            } set {
-                username = value;
-            }
-        }
-        public string MyPassword {
-            get {
-                return password;
-            } set {
-                password = value;
-            }
-        }
-    }
+namespace abstraction {
     class Program {
-        static void Main(string[] args) {
-            User myUser = new User();
-            myUser.MyUsername = "jsmith";
-            myUser.MyPassword = "password";
-            Console.WriteLine("Username: " + myUser.MyUsername);
-            string myOutput = myUser.MyPassword;
-            Console.WriteLine(myOutput);
-            Console.WriteLine("Password: " + myUser.MyPassword);
-
+        abstract class SecretMessage {
+            public void mySecretMessage() {
+                Console.WriteLine("This is my secret message");
+            }
+        }
+        class PublicMessage : SecretMessage {
+            public void myPublicMessage() {
+                Console.WriteLine("Everyone can see this message");
+            }
+        }
+        static void MainAbstraction(string[] args) {
+            PublicMessage myPubMsg = new PublicMessage();
+            myPubMsg.myPublicMessage();
+            myPubMsg.mySecretMessage();
         }
     }
 }
--- encapsulation.cs
+++ encapsulation.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 
-namespace abstraction {
+namespace encapsulation {
+    class User {
+        private string username;
+        private string password;
+        public string MyUsername {
+            get {
+                return username;
+            } set {
+                username = value;
+            }
+        }
+        public string MyPassword {
+            get {
+                return password;
+            } set {
+                password = value;
+            }
+        }
+    }
     class Program {
-        abstract class SecretMessage {
-            public void mySecretMessage() {
-                Console.WriteLine("This is my secret message");
-            }
-        }
-        class PublicMessage : SecretMessage {
-            public void myPublicMessage() {
-                Console.WriteLine("Everyone can see this message");
-            }
-        }
-        static void MainAbstraction(string[] args) {
-            PublicMessage myPubMsg = new PublicMessage();
-            myPubMsg.myPublicMessage();
-            myPubMsg.mySecretMessage();
+        static void Main(string[] args) {
+            User myUser = new User();
+            myUser.MyUsername = "jsmith";
+            myUser.MyPassword = "password";
+            Console.WriteLine("Username: " + myUser.MyUsername);
+            string myOutput = myUser.MyPassword;
+            Console.WriteLine(myOutput);
+            Console.WriteLine("Password: " + myUser.MyPassword);
         }
     }
 }
